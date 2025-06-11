cd82cb9dbc11a9784302775d85bdd54fc06257ea
Sophbx <sg7510@nyu.edu>
2025-06-11 16:13:42 -0400

    Update data file and get data code.

---
+++ data/get_data_SPY_day.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+import pandas as pd
+
+# Download raw daily prices (NOT adjusted)
+def fetch_raw_daily_yahoo_data(symbol='SPY', start='2020-01-01', end='2025-06-10'):
+    df = yf.download(symbol, start=start, end=end, auto_adjust=False)
+    df.reset_index(inplace=True)
+    df.to_csv(f"{symbol}_day_raw.csv", index=False)
+    return df
+
+df = fetch_raw_daily_yahoo_data('SPY', '2020-01-01', '2025-06-10')
+print(df.head())
